f68c34099ed318997f7c811ebb21a855859b5e8d
Battleswab <98350243+Battleswab@users.noreply.github.com>
2023-01-15 21:57:03 -0500

    Start of Python Journey

---
+++ practice.py
@@ -0,0 +1,16 @@
+print("My first python program")
+
+x=100
+print(x)
+
+num_of_cats = 198
+num_of_cats = num_of_cats - 1
+print(num_of_cats)
+
+friends = num_of_cats
+print(friends)
+
+print(friends/5)
+
+is_active=True
+print(is_active)
