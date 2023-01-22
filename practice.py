b4a7ea12d0884f5dd5a5b2a91c07d5391a2c5fab
Battleswab <98350243+Battleswab@users.noreply.github.com>
2023-01-22 15:12:20 -0500

    More practice

--- practice.py
+++ practice.py
@@ -1,16 +1,82 @@
-print("My first python program")
+#print("My first python program")
 
 x=100
-print(x)
+# print(x)
 
 num_of_cats = 198
 num_of_cats = num_of_cats - 1
-print(num_of_cats)
+# print(num_of_cats)
 
 friends = num_of_cats
-print(friends)
+# print(friends)
 
-print(friends/5)
+# print(friends/5)
 
 is_active=True
-print(is_active)
+# print(is_active)
+
+Name = "Daisy"
+age = 30
+child = None 
+# print(child)
+# print(Name)
+
+my_other_str="a hat"
+#print(my_other_str)
+
+new_line = "hello \nworld"
+#print(new_line)
+
+new_str = "Hello\\World"
+#print(new_str)
+
+quotation = "my cat said \"meow\""
+#print(quotation)
+
+mountains = "/\\/\\/\\"
+#print(mountains)
+
+message = "Hello \n World"
+#print(message)
+
+str_one="your"
+str_two="face"
+str_three= str_one + " " + str_two
+#print(str_three)
+
+username = "Blue"
+#print("Hello there, " + username)
+
+name = "a" + "b" + "z"
+#print(name)
+
+st_one = "ice"
+st_one += " cream"
+#print(st_one)
+
+x = 10 
+formatted = f"I've told you {x} times"
+#print(formatted)
+
+
+answer = "yes"
+#print(answer)
+
+decimal = 12.56348734934
+integer = int(decimal)
+#print(integer)
+
+num = 12
+num = float(num)
+
+str(8)
+#print(str)
+
+int = "I am just a number"
+#print(int)
+
+print("How many kilometers did you cycle today?")
+kms = input()
+miles = float(kms)/1.60934
+miles = round(miles,2)
+print(f"Your {kms}km ride is {miles}mi ")
